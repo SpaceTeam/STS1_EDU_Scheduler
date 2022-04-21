b1158128ca39133077230130a7e7bf9f580960cc
Florian Guggi <flo@flow-movement.at>
2022-04-21 11:13:04 +0200

    Begin restructuring

--- src/command.rs
+++ src/command.rs
@@ -1,153 +1,147 @@
 #![allow(clippy::collapsible_if)]
+
+use std::error::Error;
 use std::fs::File;
+use std::io::prelude::*;
 use std::sync::*;
 use std::thread;
 use std::time::Duration;
-use std::io::prelude::*;
 
+/// An enum storing a command with its parameters
 pub enum Command {
-    StoreArchive,
-    ExecuteProgram,
+    StoreArchive(String, Vec<u8>),
+    ExecuteProgram(String, String),
     StopProgram,
-    ReturnResults,
+    ReturnResults(String, String),
     ListFiles,
-    UpdateTime,
+    UpdateTime(u64),
 }
 
-pub struct CommandHandler {
+pub struct StudentProgram {
     watchdog_tx: Option<mpsc::Sender<bool>>,
     watchdog_handle: Option<thread::JoinHandle<()>>,
-    student_program_running: Arc<atomic::AtomicBool>
+    student_program_running: Arc<atomic::AtomicBool>,
 }
 
-impl CommandHandler {
-    pub fn create() -> CommandHandler {
-        CommandHandler {watchdog_tx: None, watchdog_handle: None, student_program_running: Arc::new(atomic::AtomicBool::new(false))}
-    }
+/// Parse a command coming from the COBC
+///
+/// `data_path` A path to the file containing the received command
+///
+/// Returns the resulting command with parameters or passes along the IO Error from file access
+pub fn process_payload(data_path: &str) -> Result<Command, std::io::Error> {
+    todo!();
+}
 
-    /// Dispatches a command to the appropriate handler with preprocessed data
-    ///
-    /// * `cmd` The received command
-    /// * `path` A path to a file with the received command data
-    pub fn dispatch_command(cmd: Command, path: String) {
-        todo!();
-    }
+/// Stores a received program in the appropriate folder and unzips it
+///
+/// * `folder` The folder to unzip into, subsequently the program id
+/// * `bytes` A vector containing the raw bytes of the zip archive
+///
+/// Returns Ok or passes along a file access/unzip process error
+pub fn store_archive(folder: &str, bytes: Vec<u8>) -> Result<(), std::io::Error> {
+    // Store bytes into temporary file
+    let mut zip_file = File::create("./data/tmp.zip")?;
+    zip_file.write_all(&bytes)?;
+    zip_file.sync_all()?;
 
-    /// Stores a received program in the appropriate folder and unzips it
-    ///
-    /// * `folder` The folder to unzip into, subsequently the program id
-    /// * `bytes` A vector containing the raw bytes of the zip archive
-    pub fn store_archive(folder: &str, bytes: Vec<u8>) {
-        log::info!("Store Archive: {}", folder);
+    let exit_status = subprocess::Exec::cmd("unzip")
+        .arg("-o") // overwrite silently
+        .arg("./data/tmp.zip")
+        .arg("-d") // target directory
+        .arg(format!("./archives/{}", folder))
+        .join();
 
-        // Store bytes into temporary file
-        let mut zip_file = match File::create("./data/tmp.zip") { // TODO tmp file in /tmp ?
-            Ok(f) => f,
-            Err(e) => {
-                log::error!("Could not create zipfile: {}", e);
-                return;
-            }
-        };
-        match zip_file.write_all(&bytes).and(zip_file.flush()) {
-            Ok(_) => (),
-            Err(e) => {
-                log::error!("Could not write zipfile: {}", e);
-                return;
-            },
-        }
+    std::fs::remove_file("./data/tmp.zip")?;
 
-        match subprocess::Exec::cmd("unzip")
-            .arg("-o") // overwrite silently
-            .arg("./data/tmp.zip")
-            .arg("-d") // target directory
-            .arg(format!("./archives/{}", folder))
-            .join()
-        {
-            Ok(es) => if !es.success() {
-                log::error!("Unzip returned with: {:?}", es);
-                return;
-            }
-            Err(e) => {
-                log::error!("Unzip could not be started {}", e);
-                return;
+    if let Err(err) = exit_status {
+        match err {
+            subprocess::PopenError::IoError(e) => return Err(e),
+            _ => {
+                unreachable!() // should only appear on Exec::cmd without args
             }
         }
-        std::fs::remove_file("./data/tmp.zip").unwrap();
     }
 
-    /// Executes a students program and starts a watchdog for it
-    /// 
-    /// * `program_id` The name of the ./archives/ subfolder
-    /// * `queue_id` The first argument for the student program
-    pub fn execute_program(&mut self, program_id: &str, queue_id: &str) {
-        // Stop already running thread
-        if self.watchdog_handle.is_some() {
-            if self.watchdog_tx.take().unwrap().send(true).is_ok() {
-                log::warn!("Program already running! Terminating...");
-                self.watchdog_handle.take().unwrap().join().unwrap();
-            }
-        }
+    Ok(())
+}
 
-        log::info!("Executing program: {} with {}", program_id, queue_id);
-        // TODO config setuid
-        let config = subprocess::PopenConfig {
-            cwd: Some(format!("./archives/{}", program_id).into()),
-            ..Default::default()
-        };
-        let mut student_process = match subprocess::Popen::create(&["python", "main.py", queue_id], config) {
+/// Executes a students program and starts a watchdog for it
+///
+/// * `program_id` The name of the ./archives/ subfolder
+/// * `queue_id` The first argument for the student program
+pub fn execute_program(sp: &mut StudentProgram, program_id: &str, queue_id: &str) {
+    // Stop already running thread
+    if sp.watchdog_handle.is_some() {
+        if sp.watchdog_tx.take().unwrap().send(true).is_ok() {
+            log::warn!("Program already running! Terminating...");
+            sp.watchdog_handle.take().unwrap().join().unwrap();
+        }
+    }
+
+    log::info!("Executing program: {} with {}", program_id, queue_id);
+    // TODO config setuid
+    let config = subprocess::PopenConfig {
+        cwd: Some(format!("./archives/{}", program_id).into()),
+        ..Default::default()
+    };
+    let mut student_process =
+        match subprocess::Popen::create(&["python", "main.py", queue_id], config) {
             Ok(p) => p,
             Err(e) => {
                 log::error!("Could not start student program: {}", e);
                 return;
             }
         };
-        self.student_program_running.store(true, atomic::Ordering::Relaxed);
-        
-        let (tx, rx) = mpsc::channel();
-        self.watchdog_tx = Some(tx);
-        let wd_flag = Arc::clone(&self.student_program_running);
+    sp.student_program_running
+        .store(true, atomic::Ordering::Relaxed);
 
-        self.watchdog_handle = Some(thread::spawn(move || {
-            // TODO proper timeout
-            for _ in 0..2 {
-                if student_process.poll().is_some() { // student program terminated itself
-                    wd_flag.store(false, atomic::Ordering::Relaxed);
-                    return;
-                }
-                if rx.try_recv().is_ok() { // check for restart/stop cmd
-                    break;
-                }
-                thread::sleep(Duration::from_secs(1));
+    let (tx, rx) = mpsc::channel();
+    sp.watchdog_tx = Some(tx);
+    let wd_flag = Arc::clone(&sp.student_program_running);
+
+    sp.watchdog_handle = Some(thread::spawn(move || {
+        // TODO proper timeout
+        for _ in 0..2 {
+            if student_process.poll().is_some() {
+                // student program terminated itself
+                wd_flag.store(false, atomic::Ordering::Relaxed);
+                return;
             }
-
-            student_process.terminate().unwrap(); // SIGTERM
-            if student_process.wait_timeout(Duration::from_millis(250)).unwrap().is_none() {
-                log::warn!("Program not responding to SIGTERM, proceeding with SIGKILL");
-                student_process.kill().unwrap(); // SIGKILL if still running
+            if rx.try_recv().is_ok() {
+                // check for restart/stop cmd
+                break;
             }
-            wd_flag.store(false, atomic::Ordering::Relaxed);
-        }));
-    }
-
-    pub fn is_program_running(&self) -> bool {
-        self.student_program_running.load(atomic::Ordering::Relaxed)
-    }
-
-    pub fn stop_program(&mut self) {
-        log::info!("Stopping program...");
-        if !self.is_program_running() {
-            log::warn!("Attempting to stop program, but none is running");
-            return;
+            thread::sleep(Duration::from_secs(1));
         }
 
-        if self.watchdog_tx.is_some() {
-            self.watchdog_tx.as_ref().unwrap().send(true).expect("is_program_running() == true, but watchdog channel is dead");
-            self.watchdog_handle.take()
-                .expect("is_program_running() == true, but watchdog handle is none")
-                .join().unwrap();
+        student_process.terminate().unwrap(); // SIGTERM
+        if student_process
+            .wait_timeout(Duration::from_millis(250))
+            .unwrap()
+            .is_none()
+        {
+            log::warn!("Program not responding to SIGTERM, proceeding with SIGKILL");
+            student_process.kill().unwrap(); // SIGKILL if still running
         }
-        else {
-            panic!("is_program_running() == true, but watchdog channel is none");
-        }
+        wd_flag.store(false, atomic::Ordering::Relaxed);
+    }));
+}
+
+pub fn stop_program(sp: &mut StudentProgram) {
+    log::info!("Stopping program...");
+
+    if sp.watchdog_tx.is_some() {
+        sp.watchdog_tx
+            .as_ref()
+            .unwrap()
+            .send(true)
+            .expect("is_program_running() == true, but watchdog channel is dead");
+        sp.watchdog_handle
+            .take()
+            .expect("is_program_running() == true, but watchdog handle is none")
+            .join()
+            .unwrap();
+    } else {
+        panic!("is_program_running() == true, but watchdog channel is none");
     }
 }
--- tests/command_tests.rs
+++ tests/command_tests.rs
@@ -1,4 +1,4 @@
-use STS1_EDU_Scheduler::command::*;
+use STS1_EDU_Scheduler::command;
 use std::io::prelude::*;
 use simplelog as sl;
 
@@ -17,7 +17,7 @@ fn store_archive() {
     setup();
     let mut buf = Vec::new();
     std::fs::File::open("./tests/student_program.zip").unwrap().read_to_end(&mut buf).unwrap();
-    CommandHandler::store_archive("testa", buf);
+    command::store_archive("testa", buf);
 
     assert_eq!(0, std::process::Command::new("diff")
         .args(["-yq", "--strip-trailing-cr", "tests/test_data", "archives/testa"])
