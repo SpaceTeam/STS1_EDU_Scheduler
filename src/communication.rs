3c2b0512021b8f8bb779060556b77a6afb4f180e
Florian Guggi <flo@flow-movement.at>
2022-06-15 18:07:24 +0200

    Adjusted communication trait

--- src/communication.rs
+++ src/communication.rs
@@ -4,15 +4,20 @@ use std::sync;
 use std::path;
 use std::error::Error;
 
+type Result<T> = std::result::Result<T, Box<dyn Error>>;
 pub trait CommunicationHandle {
     /// Sends the bytes to the COBC, packaged accordingly
-    fn send(&mut self, bytes: Vec<u8>);
+    fn send(&mut self, bytes: Vec<u8>) -> Result<()>;
     /// Blocks until a command from the COBC is received. Returns the raw bytes
-    fn receive(&self) -> Result<Vec<u8>, Box<dyn Error>>;
+    fn receive(&self) -> Result<Vec<u8>>;
     /// Sends a NACK to the COBC
-    fn send_nack(&self);
+    fn send_nack(&mut self) -> Result<()> {
+        return self.send(vec![0x33])
+    }
     /// Sends a ACK to the COBC
-    fn send_ack(&self);
+    fn send_ack(&mut self) -> Result<()> {
+        return self.send(vec![0x55])
+    }
 }
 
 pub struct UARTHandle {
@@ -25,19 +30,11 @@ impl UARTHandle {
 }
 
 impl CommunicationHandle for UARTHandle {
-    fn send(&mut self, bytes: Vec<u8>) {
+    fn send(&mut self, bytes: Vec<u8>) -> Result<()> {
         todo!();
     }
 
-    fn receive(&self) -> Result<Vec<u8>, Box<dyn Error>> {
-        todo!();
-    }
-
-    fn send_ack(&self) {
-        todo!();
-    }
-
-    fn send_nack(&self) {
+    fn receive(&self) -> Result<Vec<u8>> {
         todo!();
     }
 }
