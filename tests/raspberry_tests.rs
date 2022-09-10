1a2b57ff115687a8c739b3c67e6263bb5a2e307a
Florian Guggi <flo@flow-movement.at>
2022-09-10 02:33:12 +0200

    Add pack examples

---
+++ tests/raspberry_tests.rs
@@ -0,0 +1,34 @@
+use STS1_EDU_Scheduler::communication::{CSBIPacket, CSBIPacket::*};
+
+
+/// These tests are for use with the actual raspberry hardware
+/// 
+/// Use `cargo test --features rpi` to run them
+
+#[cfg(feature = "rpi")]
+#[test]
+fn build_pack_store_archive() {
+    let packets = vec![ // Create a list of packets to send
+        DATA(vec![1,0,0]), // Store Archive
+        DATA(std::fs::read("tests/student_program.zip").unwrap()), // bytes of the zip
+        EOF // End of File
+    ];
+
+    std::fs::write("tests/store_archive.pack", combine_packets(packets)); // Save the packets into a .pack file
+}
+
+#[cfg(feature = "rpi")]
+#[test]
+fn build_pack_execute_program() {
+    // Execute with Program ID 0 & Queue ID 0
+    std::fs::write("tests/execute_program.pack", DATA(vec![2, 0, 0, 0, 0]).serialize());
+}
+
+
+fn combine_packets(list: Vec<CSBIPacket>) -> Vec<u8> {
+    let mut v = Vec::new();
+    for p in list {
+        v.extend(p.serialize());
+    }
+    v
+}
