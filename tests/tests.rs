5d0983734d5d54ce56e72627e5a850931638e6f1
Florian Guggi <flo@flow-movement.at>
2022-10-07 02:32:13 +0200

    move software tests into subfolder

---
+++ tests/tests.rs
@@ -0,0 +1,8 @@
+#[path = "software_tests/command_integration.rs"]
+mod command_integration;
+
+#[path = "software_tests/communication_tests.rs"]
+mod communication_tests;
+
+#[path = "software_tests/raspberry_tests.rs"]
+mod raspberry_tests;
