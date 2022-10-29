55ffb395461027402a141652ee0f72d479a47e1b
Nico <ferreira.rambo95@gmail.com>
2022-10-29 18:30:53 +0200

    made uart panic mgs visible and new test packs

--- tests/software_tests/raspberry_tests.rs
+++ tests/software_tests/raspberry_tests.rs
@@ -7,11 +7,13 @@ use STS1_EDU_Scheduler::communication::{CSBIPacket, CSBIPacket::*};
 #[cfg(feature = "rpi")]
 #[test]
 fn build_pack_store_archive() {
+    
     let packets = vec![
         // Create a list of packets to send
         DATA(vec![1, 0, 0]), // Store Archive
         DATA(std::fs::read("tests/student_program.zip").unwrap()), // bytes of the zip
         EOF,                 // End of File
+        
     ];
 
     std::fs::write("tests/store_archive.pack", combine_packets(packets)); // Save the packets into a .pack file
@@ -21,8 +23,22 @@ fn build_pack_store_archive() {
 #[test]
 fn build_pack_execute_program() {
     // Execute with Program ID 0 & Queue ID 0
-    std::fs::write("tests/execute_program.pack", DATA(vec![2, 0, 0, 0, 0]).serialize());
+    std::fs::write("tests/execute_program.pack", DATA(vec![2, 0, 0, 0, 0, 5, 0]).serialize());
 }
+
+#[cfg(feature = "rpi")]
+#[test]
+fn build_pack_get_status() {
+    std::fs::write("tests/get_status.pack", DATA(vec![4]).serialize());
+}
+
+#[cfg(feature = "rpi")]
+#[test]
+fn build_pack_return_result()
+{
+    std::fs::write("tests/return_result.pack", DATA(vec![5]).serialize());
+}
+
 
 fn combine_packets(list: Vec<CSBIPacket>) -> Vec<u8> {
     let mut v = Vec::new();
