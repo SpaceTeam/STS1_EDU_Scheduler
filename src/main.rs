ca5033d3f78da818d0268000353eb17ece781f3b
Florian Guggi <flor.guggi@gmail.com>
2022-12-03 01:24:25 +0100

    reorder code and document code in main.rs

--- src/main.rs
+++ src/main.rs
@@ -14,16 +14,22 @@ mod persist;
 use command::CommandError;
 use communication::CommunicationError;
 
+const HEARTBEAT_PIN: u8 = 34;
+const HEARTBEAT_FREQ: u64 = 10; //Hz
+const UPDATE_PIN: u8 = 35;
+
 fn main() {
+    // write all logging into a file
     let _ = sl::WriteLogger::init(
         sl::LevelFilter::Info,
         sl::Config::default(),
         std::fs::File::create("log").unwrap(),
     );
 
-    const UPDATE_PIN: u8 = 35;
-
+    // construct a wrapper for UART communication
     let mut com = communication::UARTHandle::new(921600);
+
+    // construct a wrapper for resources that are shared between different commands
     let ec = command::ExecutionContext::new(
         "./data/status_queue".into(),
         "./data/result_queue".into(),
@@ -32,24 +38,10 @@ fn main() {
     .unwrap();
     let mut exec = Arc::new(Mutex::new(ec));
 
-    //Heartbeat thread
-    thread::spawn(|| {
-        const HEARTBEAT_FREQ: u64 = 10; //Hz
-        const HEARTBEAT_PIN: u8 = 34;
-        const TOGGLE_TIME_MS: time::Duration =
-            time::Duration::from_millis((1000 / HEARTBEAT_FREQ / 2) as u64);
+    // start a thread that will update the heartbeat pin
+    thread::spawn(heartbeat_loop);
 
-        let gpio = Gpio::new().unwrap();
-        let mut pin = gpio.get(HEARTBEAT_PIN).unwrap().into_output();
-
-        loop {
-            pin.set_high();
-            thread::sleep(TOGGLE_TIME_MS);
-            pin.set_low();
-            thread::sleep(TOGGLE_TIME_MS);
-        }
-    });
-
+    // main loop
     loop {
         let ret = command::handle_command(&mut com, &mut exec);
 
@@ -85,5 +77,20 @@ fn handle_communication_error(ce: CommunicationError) {
             log::error!("Communication timed out");
         }
         CommunicationError::CRCError => (),
+    }
+}
+
+fn heartbeat_loop() -> ! {
+    const TOGGLE_TIME_MS: time::Duration =
+        time::Duration::from_millis((1000 / HEARTBEAT_FREQ / 2) as u64);
+
+    let gpio = Gpio::new().unwrap();
+    let mut pin = gpio.get(HEARTBEAT_PIN).unwrap().into_output();
+
+    loop {
+        pin.set_high();
+        thread::sleep(TOGGLE_TIME_MS);
+        pin.set_low();
+        thread::sleep(TOGGLE_TIME_MS);
     }
 }
