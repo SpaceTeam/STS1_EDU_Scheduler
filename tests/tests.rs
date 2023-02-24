d938db32cd69e287157e742a00d68470bcce6450
Florian Guggi <flor.guggi@gmail.com>
2023-02-24 07:13:08 +0100

    move tests for commands into their own files for better visibility

--- tests/tests.rs
+++ tests/tests.rs
@@ -4,5 +4,17 @@ mod command_integration;
 #[path = "software_tests/communication_tests.rs"]
 mod communication_tests;
 
-#[path = "software_tests/raspberry_tests.rs"]
-mod raspberry_tests;
+#[path = "software_tests/store_archive.rs"]
+mod store_archive;
+
+#[path = "software_tests/execute_program.rs"]
+mod execute_program;
+
+#[path = "software_tests/stop_program.rs"]
+mod stop_program;
+
+#[path = "software_tests/get_status.rs"]
+mod get_status;
+
+#[path = "software_tests/return_result.rs"]
+mod return_result;
